fed82e0173ef8b96837c0a81ccb166764050b234
Jeff McMorris <delay@pobox.com>
2021-04-13 20:09:51 -0500

    create code generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,172 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:csv/csv.dart';
 
 void main() {
-  runApp(MyApp());
+  updateLocalizationFile();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+Future updateLocalizationFile() async {
+  //if user fails to add arguments to commandline output error message
+  //the document id for your google sheet
+  String documentId = "1oS7iJ6ocrZBA53SxRfKF0CG9HAaXeKtzvsTBhgG4Zzk";
+  //the sheetid of your google sheet
+  String sheetId = "0";
+
+  String _phraseKey = '';
+  List<LocalizationModel> _localizations = [];
+  String _localizationFile = """import 'package:get/get.dart';
+
+class Localization extends Translations {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  Map<String, Map<String, String>> get keys => {
+    """;
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  try {
+    //query google voice api and get response which is base 64 encoded.
+    final url =
+        'https://docs.google.com/spreadsheets/d/$documentId/export?format=csv&id=$documentId&gid=$sheetId';
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    stdout.writeln('');
+    stdout.writeln('---------------------------------------');
+    stdout.writeln('Downloading Google sheet url "$url" ...');
+    stdout.writeln('---------------------------------------');
+    //decode base64 file and save as binary audio file (mp3)
+    var response = await http
+        .get(Uri.parse(url), headers: {'accept': 'text/csv;charset=UTF-8'});
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    // print('Google sheet csv:\n ${response.body}');
 
-  final String title;
+    final bytes = response.bodyBytes.toList();
+    final csv = Stream<List<int>>.fromIterable([bytes]);
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+    final fields = await csv
+        .transform(utf8.decoder)
+        .transform(CsvToListConverter(
+          shouldParseNumbers: false,
+        ))
+        .toList();
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+    final index = fields[0]
+        .cast<String>()
+        .map(_uniformizeKey)
+        .takeWhile((x) => x.isNotEmpty)
+        .toList();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    for (var r = 1; r < fields.length; r++) {
+      final rowValues = fields[r];
+
+      /// Creating a map
+      final row = Map<String, String>.fromEntries(
+        rowValues
+            .asMap()
+            .entries
+            .where(
+              (e) => e.key < index.length,
+            )
+            .map(
+              (e) => MapEntry(index[e.key], e.value),
+            ),
+      );
+
+      row.forEach((key, value) {
+        if (key == 'key') {
+          _phraseKey = value;
+        } else {
+          bool _languageAdded = false;
+          _localizations.forEach((element) {
+            if (element.language == key) {
+              element.phrases.add(PhraseModel(key: _phraseKey, phrase: value));
+              _languageAdded = true;
+            }
+          });
+          if (_languageAdded == false) {
+            _localizations.add(LocalizationModel(
+                language: key,
+                phrases: [PhraseModel(key: _phraseKey, phrase: value)]));
+          }
+        }
+      });
+    }
+
+    _localizations.forEach((_localization) {
+      String _language = _localization.language;
+      String _currentLanguageTextCode = "'$_language': {\n";
+      _localizationFile = _localizationFile + _currentLanguageTextCode;
+      _localization.phrases.forEach((_phrase) {
+        String _phraseKey = _phrase.key;
+        String _phrasePhrase = _phrase.phrase.replaceAll(r"'", "\\\'");
+        String _currentPhraseTextCode = "'$_phraseKey': '$_phrasePhrase',\n";
+        _localizationFile = _localizationFile + _currentPhraseTextCode;
+      });
+      String _currentLanguageCodeEnding = "},\n";
+      _localizationFile = _localizationFile + _currentLanguageCodeEnding;
     });
-  }
+    String _fileEnding = """
+        };
+      }
+      """;
+    _localizationFile = _localizationFile + _fileEnding;
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    stdout.writeln('');
+    stdout.writeln('---------------------------------------');
+    stdout.writeln('Saving localization.g.dart');
+    stdout.writeln('---------------------------------------');
+    final file = File('localization.g.dart');
+    await file.writeAsString(_localizationFile);
+    stdout.writeln('Done...');
+  } catch (e) {
+    //output error
+    stderr.writeln('error: networking error');
+    stderr.writeln(e.toString());
+  }
+}
+
+String _uniformizeKey(String key) {
+  key = key.trim().replaceAll('\n', '').toLowerCase();
+  return key;
+}
+
+//Localization Model
+class LocalizationModel {
+  final String language;
+  final List<PhraseModel> phrases;
+
+  LocalizationModel({
+    required this.language,
+    required this.phrases,
+  });
+
+  factory LocalizationModel.fromMap(Map data) {
+    return LocalizationModel(
+      language: data['language'],
+      phrases:
+          (data['phrases'] as List).map((v) => PhraseModel.fromMap(v)).toList(),
     );
   }
+
+  Map<String, dynamic> toJson() => {
+        "language": language,
+        "phrases": List<dynamic>.from(phrases.map((x) => x.toJson())),
+      };
+}
+
+class PhraseModel {
+  String key;
+  String phrase;
+
+  PhraseModel({required this.key, required this.phrase});
+
+  factory PhraseModel.fromMap(Map data) {
+    return PhraseModel(
+      key: data['key'],
+      phrase: data['phrase'] ?? '',
+    );
+  }
+  Map<String, dynamic> toJson() => {
+        "key": key,
+        "phrase": phrase,
+      };
 }
